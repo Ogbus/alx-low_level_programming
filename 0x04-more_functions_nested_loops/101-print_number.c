d12374ccf987ec722f58221d0a50b9ec64628117
Ogbuluijah Owaji-inyagham <felixogbuluijah@yahoo.com>
2022-04-14 06:12:13 -0700

    Commit message for task thirteen

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,6 +6,17 @@
 */
 void print_number(int n)
 {
-_putchar("%d", n);
-_putchar("\n");
+
+if (n < 0)
+{
+_putchar('-');
+n *= -1;
+}
+
+if (n / 10 != 0)
+{
+print_number(n / 10);
+}
+
+_putchar('0' + (n % 10));
 }
